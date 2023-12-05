9cd70ec02c7ba379a2163d17858bc9b41f58e62e
davidtipe17 <davidmarcotipe@gmail.com>
2023-12-05 12:02:47 -0500

    challenge cambios

--- src/pages/Vista1.jsx
+++ src/pages/Vista1.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Page from '../components/Page';
-import './Vista'
+import './Vista.css'
 const Vista1 = () => {
   return (
     <Page>
       <div className="vista1-container">
-        <h1 className="vista1-title">Hola, esta es la Vista 1</h1>
+        <h1 className="vista1-title">Hola!, esta es la Vista 1</h1>
         <p className="vista1-content">
-          Contenido de la Vista 1
         </p>
       </div>
     </Page>
--- src/pages/Vista2.jsx
+++ src/pages/Vista2.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Page from '../components/Page';
 import './Vista.css'
-
-
 const Vista2 = () => {
   return (
     <Page>
       <div className="vista1-container">
-        <h1 className="vista1-title">Hola, esta es la Vista 2</h1>
+        <h1 className="vista1-title">Hola!, esta es la Vista 2</h1>
         <p className="vista1-content">
-          Contenido de la Vista 2
+         
         </p>
       </div>
     </Page>
